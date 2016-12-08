0cba62efe5e3aab52bdc50286369431ad274012e
0xFireball <0xFireball@outlook.com>
2016-12-07 18:44:37 -0800

    ignore and add litedb database access

---
+++ Server/dotnet/BetaGo.Server/src/BetaGo.Server/Services/Database/DatabaseAccessService.cs
@@ -0,0 +1,15 @@
+﻿using LiteDB;
+
+namespace BetaGo.Server.Services.Database
+{
+    public class DatabaseAccessService
+    {
+        public static string UsersCollectionDatabaseKey => "Users";
+
+        public LiteDatabase OpenOrCreateDefault()
+        {
+            //notebin3.lidb
+            return new LiteDatabase("betago_srv.lidb");
+        }
+    }
+}
