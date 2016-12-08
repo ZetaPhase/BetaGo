a241e62e6e7da45acaf20b84b8a48edec11ad601
0xFireball <0xFireball@outlook.com>
2016-12-07 18:31:56 -0800

    set up NancyFx

--- Server/dotnet/BetaGo.Server/src/BetaGo.Server/Program.cs
+++ Server/dotnet/BetaGo.Server/src/BetaGo.Server/Program.cs
@@ -1,23 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-
-namespace BetaGo.Server
+﻿namespace BetaGo.Server
 {
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
+    using System.IO;
+
     public class Program
     {
         public static void Main(string[] args)
         {
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .UseStartup<Startup>()
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddCommandLine(args)
+                .AddJsonFile(Path.Combine(Directory.GetCurrentDirectory(), "hosting.json"), false)
                 .Build();
 
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseConfiguration(config)
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseStartup<Startup>()
+                .Build();
             host.Run();
         }
     }
